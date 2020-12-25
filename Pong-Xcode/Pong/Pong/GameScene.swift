55951360598eeafba5e7f6cc904bc7e63b314f8f
Jaspreet Singh <41412193+Jaspreet99@users.noreply.github.com>
2020-12-25 18:31:02 +0530

    Add files via upload

---
+++ Pong-Xcode/Pong/Pong/GameScene.swift
@@ -0,0 +1,73 @@
+//
+//  GameScene.swift
+//  Pong
+//
+//  Created by Jaspreet Singh on 14/04/20.
+//  Copyright © 2020 Jaspreet Singh. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+         var ball = SKSpriteNode()
+         var enemy = SKSpriteNode()
+         var main = SKSpriteNode()
+    
+    var score = [Int]()
+    
+    
+    override func didMove(to view: SKView) {
+        StartGame()
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+    }
+    func StartGame() {
+        score = [0,0]
+        }
+    func addScore(playerwhoWon:SKSpriteNode){
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        if playerwhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+
+        }
+        else if playerwhoWon == enemy{
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            main.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let location = touch.location(in: self)
+            main.run(SKAction.moveTo(x: location.x, duration: 0.1))
+        }
+    }
+    override func update(_ currentTime: TimeInterval) {
+        
+        // Called before each frame is rendered
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        if ball.position.y <= main.position.y - 70 {
+            addScore(playerwhoWon: enemy)
+        }
+        else if ball.position.y >= enemy.position.y + 70{
+            addScore(playerwhoWon: main)
+        }
+    }
+}
